c1158188decdb480ca572a767435b33a58e2802f
Mike Barberry <mikebarberry@protonmail.com>
2020-09-30 03:44:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@ def line(array)
   if array.length < 1
     puts "The line is currently empty."
   else
-    puts "The line is currently: " array.each_with_index do |element, index|
+    puts "The line is currently: "
+    array.each_with_index do |element, index|
       puts "#{index + 1}: #{element}"
 #    array.each do |element, index|
 #      puts (index + 1) + element
