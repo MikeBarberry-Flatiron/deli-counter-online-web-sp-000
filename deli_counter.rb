425f93b2c8666c07cc538bb691720581b2b84dab
Mike Barberry <mikebarberry@protonmail.com>
2020-09-30 03:15:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 # Write your code here.
 def line(array)
   if array.length < 1
-    return "The line is currently empty."
+    puts "The line is currently empty."
   else
     return array.join(", ")
-  end 
+  end
 end
