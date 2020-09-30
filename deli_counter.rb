9322f935dcc4b820ca4c4629957136a6cc6316ca
Mike Barberry <mikebarberry@protonmail.com>
2020-09-30 04:54:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,9 +19,10 @@ end
 def now_serving(arr)
   if arr.length == 0
     puts "There is nobody waiting to be served."
-  end
-  puts "Currently serving #{arr[0]}."
-  arr.shift
+  else 
+    puts "Currently serving #{arr[0]}."
+    arr.shift
+  end 
 end
 
 line = ["Jim", "Tim", "Stephanie", "Elton"]
